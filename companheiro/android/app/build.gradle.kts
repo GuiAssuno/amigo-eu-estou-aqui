2bb492b9d9fdae0e79515bf07182074753d99a3f
GuiAssuno <gui_ab_@hotmail.com>
2026-05-23 20:06:27 -0300

    att id .kts

--- companheiro/android/app/build.gradle.kts
+++ companheiro/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.projeto.companheiro"
+    namespace = "com.example.companheiro"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.projeto.companheiro"
+        applicationId = "com.example.companheiro"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
